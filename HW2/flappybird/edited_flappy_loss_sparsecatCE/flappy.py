9a6d2a5cd8e11964f1479bfd73f16f6d2ef17969
Natalie Rakoski <nrakoski3@gatech.edu>
2017-10-20 19:47:38 -0400

    try revert

--- HW2/flappybird/edited_flappy_loss_sparsecatCE/flappy.py
+++ HW2/flappybird/edited_flappy_loss_sparsecatCE/flappy.py
@@ -119,35 +119,35 @@ for i in range(total_models):
 PLAYERS_LIST = (
     # red bird
     (
-        os.path.join('assets/sprites/', 'redbird-upflap.png'),
-        os.path.join('assets/sprites/', 'redbird-midflap.png'),
-        os.path.join('assets/sprites/', 'redbird-downflap.png'),
+        'assets/sprites/redbird-upflap.png',
+        'assets/sprites/redbird-midflap.png',
+        'assets/sprites/redbird-downflap.png',
     ),
     # blue bird
     (
         # amount by which base can maximum shift to left
-        os.path.join('assets/sprites/', 'bluebird-upflap.png'),
-        os.path.join('assets/sprites/', 'bluebird-midflap.png'),
-        os.path.join('assets/sprites/', 'bluebird-downflap.png'),
+        'assets/sprites/bluebird-upflap.png',
+        'assets/sprites/bluebird-midflap.png',
+        'assets/sprites/bluebird-downflap.png',
     ),
     # yellow bird
     (
-        os.path.join('assets/sprites/', 'yellowbird-upflap.png'),
-        os.path.join('assets/sprites/', 'yellowbird-midflap.png'),
-        os.path.join('assets/sprites/', 'yellowbird-downflap.png'),
+        'assets/sprites/yellowbird-upflap.png',
+        'assets/sprites/yellowbird-midflap.png',
+        'assets/sprites/yellowbird-downflap.png',
     ),
 )
 
 # list of backgrounds
 BACKGROUNDS_LIST = (
-    os.path.join('assets/sprites/', 'background-day.png'),
-    os.path.join('assets/sprites/', 'background-night.png'),
+    'assets/sprites/background-day.png',
+    'assets/sprites/background-night.png',
 )
 
 # list of pipes
 PIPES_LIST = (
-    os.path.join('assets/sprites/', 'pipe-green.png'),
-    os.path.join('assets/sprites/', 'pipe-red.png'),
+    'assets/sprites/pipe-green.png',
+    'assets/sprites/pipe-red.png',
 )
 
 
@@ -160,24 +160,24 @@ def main():
 
     # numbers sprites for score display
     IMAGES['numbers'] = (
-        pygame.image.load_basic(os.path.join('assets/sprites/', '0.png')).convert_alpha(),
-        pygame.image.load_basic(os.path.join('assets/sprites/', '1.png')).convert_alpha(),
-        pygame.image.load_basic(os.path.join('assets/sprites/', '2.png')).convert_alpha(),
-        pygame.image.load_basic(os.path.join('assets/sprites/', '3.png')).convert_alpha(),
-        pygame.image.load_basic(os.path.join('assets/sprites/', '4.png')).convert_alpha(),
-        pygame.image.load_basic(os.path.join('assets/sprites/', '5.png')).convert_alpha(),
-        pygame.image.load_basic(os.path.join('assets/sprites/', '6.png')).convert_alpha(),
-        pygame.image.load_basic(os.path.join('assets/sprites/', '7.png')).convert_alpha(),
-        pygame.image.load_basic(os.path.join('assets/sprites/', '8.png')).convert_alpha(),
-        pygame.image.load_basic(os.path.join('assets/sprites/', '9.png')).convert_alpha()
+        pygame.image.load('assets/sprites/0.png').convert_alpha(),
+        pygame.image.load('assets/sprites/1.png').convert_alpha(),
+        pygame.image.load('assets/sprites/2.png').convert_alpha(),
+        pygame.image.load('assets/sprites/3.png').convert_alpha(),
+        pygame.image.load('assets/sprites/4.png').convert_alpha(),
+        pygame.image.load('assets/sprites/5.png').convert_alpha(),
+        pygame.image.load('assets/sprites/6.png').convert_alpha(),
+        pygame.image.load('assets/sprites/7.png').convert_alpha(),
+        pygame.image.load('assets/sprites/8.png').convert_alpha(),
+        pygame.image.load('assets/sprites/9.png').convert_alpha()
     )
 
     # game over sprite
-    IMAGES['gameover'] = pygame.image.load_basic(os.path.join('assets/sprites/', 'gameover.png')).convert_alpha()
+    IMAGES['gameover'] = pygame.image.load('assets/sprites/gameover.png').convert_alpha()
     # message sprite for welcome screen
-    IMAGES['message'] = pygame.image.load_basic(os.path.join('assets/sprites/', 'message.png')).convert_alpha()
+    IMAGES['message'] = pygame.image.load('assets/sprites/message.png').convert_alpha()
     # base (ground) sprite
-    IMAGES['base'] = pygame.image.load_basic(os.path.join('assets/sprites/', 'base.png')).convert_alpha()
+    IMAGES['base'] = pygame.image.load('assets/sprites/base.png').convert_alpha()
 
     # # sounds
     # if 'win' in sys.platform:
@@ -194,22 +194,22 @@ def main():
     while True:
         # select random background sprites
         randBg = random.randint(0, len(BACKGROUNDS_LIST) - 1)
-        IMAGES['background'] = pygame.image.load_basic(BACKGROUNDS_LIST[randBg]).convert()
+        IMAGES['background'] = pygame.image.load(BACKGROUNDS_LIST[randBg]).convert()
 
         # select random player sprites
         randPlayer = random.randint(0, len(PLAYERS_LIST) - 1)
         IMAGES['player'] = (
-            pygame.image.load_basic(PLAYERS_LIST[randPlayer][0]).convert_alpha(),
-            pygame.image.load_basic(PLAYERS_LIST[randPlayer][1]).convert_alpha(),
-            pygame.image.load_basic(PLAYERS_LIST[randPlayer][2]).convert_alpha(),
+            pygame.image.load(PLAYERS_LIST[randPlayer][0]).convert_alpha(),
+            pygame.image.load(PLAYERS_LIST[randPlayer][1]).convert_alpha(),
+            pygame.image.load(PLAYERS_LIST[randPlayer][2]).convert_alpha(),
         )
 
         # select random pipe sprites
         pipeindex = random.randint(0, len(PIPES_LIST) - 1)
         IMAGES['pipe'] = (
             pygame.transform.rotate(
-                pygame.image.load_basic(PIPES_LIST[pipeindex]).convert_alpha(), 180),
-            pygame.image.load_basic(PIPES_LIST[pipeindex]).convert_alpha(),
+                pygame.image.load(PIPES_LIST[pipeindex]).convert_alpha(), 180),
+            pygame.image.load(PIPES_LIST[pipeindex]).convert_alpha(),
         )
 
         # hismask for pipes
